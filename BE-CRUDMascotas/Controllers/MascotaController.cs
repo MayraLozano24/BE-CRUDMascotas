7fa462c9d4373f203e3c79d51e815637f8fc69d6
Mayra Lozano <mayra_2493@hotmail.com>
2022-12-22 12:38:24 -0500

    Agregar archivos de proyecto.

---
+++ BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,98 @@
+﻿using BE_CRUDMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+
+        public MascotaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMascota = await _context.Mascota.ToListAsync();
+                return Ok(listMascota);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var mascota = await _context.Mascota.FindAsync(id);
+
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+                return Ok(mascota);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var mascota = await _context.Mascota.FindAsync(id);
+
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Mascota.Remove(mascota);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Mascota mascota)
+        {
+            try
+            {
+                mascota.FechaCreacion = DateTime.Now;
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("Get", new {id = mascota.Id }, mascota);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+    }   
+
+}
